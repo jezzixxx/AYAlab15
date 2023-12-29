7fe102276fbe26598d061f3721414cbf45cfc8ae
jezzixxx <pkapav02@gmail.com>
2023-12-29 17:15:28 +0300

    Добавьте файлы проекта.

---
+++ Task3.cs
@@ -0,0 +1,69 @@
+﻿//using System;
+//using System.IO;
+//using System.Xml;
+//using System.Text.Json;
+
+//namespace lab15
+//{
+//    internal class Task3
+//    {
+//        public class SingleRandomizer
+//        {
+//            private static SingleRandomizer instance;
+//            private static readonly object lockObject = new object();
+
+//            private Random random;
+
+//            private SingleRandomizer()
+//            {
+//                random = new Random();
+//            }
+
+//            public static SingleRandomizer Instance
+//            {
+//                get
+//                {
+//                    if (instance == null)
+//                    {
+//                        lock (lockObject)
+//                        {
+//                            if (instance == null)
+//                            {
+//                                instance = new SingleRandomizer();
+//                            }
+//                        }
+//                    }
+//                    return instance;
+//                }
+//            }
+
+//            public int Next()
+//            {
+//                lock (lockObject)
+//                {
+//                    return random.Next();
+//                }
+//            }
+//        }
+
+//        public class Program
+//        {
+//            public static void Main(string[] args)
+//            {
+//                int numThreads = 5;
+
+//                Thread[] threads = new Thread[numThreads];
+//                for (int i = 0; i < numThreads; i++)
+//                {
+//                    threads[i] = new Thread(() =>
+//                    {
+//                        SingleRandomizer randomizer = SingleRandomizer.Instance;
+//                        int randomNumber = randomizer.Next();
+//                        Console.WriteLine($"Thread ID: {Thread.CurrentThread.ManagedThreadId}, Random Number: {randomNumber}");
+//                    });
+//                    threads[i].Start();
+//                }
+//            }
+//        }
+//    }
+//}
